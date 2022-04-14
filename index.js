c7c8518acb84c021469d6ccc1eb7e37fe6d6a126
Shontymohite <94635317+Shontymohite@users.noreply.github.com>
2022-04-14 23:04:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var num = Math.floor(Math.random()*6)+1;
 var randomdiceimage = "dice" + num + ".png";
-var diceimage = "images/" + randomdiceimage;
 var image = document.querySelectorAll("img")[0];
-image.setAttribute("src" , diceimage);
+image.setAttribute("src" , randomdiceimage);
 
 var num2 = Math.floor(Math.random() * 6) + 1;
 var randomdiceimage2 = "dice" + num2 + ".png";
-var diceimage2 = "images/" + randomdiceimage2;
+
 var image = document.querySelectorAll("img")[1];
-image.setAttribute("src" , diceimage2);
+image.setAttribute("src" , randomdiceimage2);
 
 if(num > num2){
     document.querySelector("h1").innerHTML = "🚩 Player 1 Wins...!"
